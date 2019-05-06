8d36bff1ee4240dcad943b8973395a2a1d6680ed
Matheus Bernardo <matheussber@aluno.unb.br>
2019-05-06 16:53:49 -0300

    Adds visitor class that transforms CST into an AST that represents a FRED value

--- fred.js
+++ fred.js
@@ -24,7 +24,8 @@ const StringLiteral = createToken({
 
 const BlobString = createToken({
     name: "BlobString",
-    pattern: /"(?:[^\\"\u\U]|\\(?:[bfnrtv"\\/]|x[0-9a-fA-F]))*"/
+    pattern: /"(?:[^\\"\u\U]|\\(?:[bfnrtv"\\/]|x[0-9a-fA-F]))*"/,
+    line_breaks: false
 })
 
 const QuotedVariable = createToken({
@@ -63,6 +64,12 @@ const WhiteSpace = createToken({
     group: Lexer.SKIPPED
 })
 
+const Comment = createToken({
+    name: "Comment",
+    pattern: /;.*/,
+    group: Lexer.SKIPPED
+})
+
 const DateFormat = createToken({
     name: "DateFormat",
     pattern: /\d{4}-\d{2}-\d{2}/
@@ -80,11 +87,11 @@ const TimeOffSet = createToken({
 
 const allTokens = [
     WhiteSpace,
+    Comment,
     NumberLiteral,
     HexLiteral,
     OctLiteral,
     BinLiteral,
-    QuotedVariable,
     StringLiteral,
     BlobString,
     DateFormat,
@@ -105,7 +112,8 @@ const allTokens = [
     True,
     False,
     Null,
-    Variable
+    Variable,
+    QuotedVariable
 ]
 
 const FREDLexer = new Lexer(allTokens);
@@ -201,7 +209,7 @@ class FREDParser extends Parser {
         })
 
         $.RULE("bool", () => {
-            $.OR([{ ALT: $.CONSUME(True) }, { ALT: $.CONSUME(False) }])
+            $.OR([{ ALT: () => $.CONSUME(True) }, { ALT: () => $.CONSUME(False) }])
         })
 
         $.RULE("symbol", () => {
@@ -212,7 +220,7 @@ class FREDParser extends Parser {
         $.RULE("dateOrDateTime", () => {
             $.CONSUME(DateFormat)
             $.OPTION(() => {
-                $.OR([{ ALT: $.CONSUME(T) }, { ALT: $.CONSUME(_) }])
+                $.OR([{ ALT: () => $.CONSUME(T) }, { ALT: () => $.CONSUME(_) }])
                 $.CONSUME(Time)
                 $.OPTION1(() => { $.CONSUME(TimeOffSet) })
             })
@@ -220,15 +228,15 @@ class FREDParser extends Parser {
 
         $.RULE("number", () => {
             $.OR([
-                { ALT: $.CONSUME(NumberLiteral) },
-                { ALT: $.CONSUME(HexLiteral) },
-                { ALT: $.CONSUME(OctLiteral) },
-                { ALT: $.CONSUME(BinLiteral) }
+                { ALT: () => $.CONSUME(NumberLiteral) },
+                { ALT: () => $.CONSUME(HexLiteral) },
+                { ALT: () => $.CONSUME(OctLiteral) },
+                { ALT: () => $.CONSUME(BinLiteral) }
             ])
         })
 
         $.RULE("string", () => {
-            $.OR([{ ALT: $.CONSUME(StringLiteral) }, { ALT: $.SUBRULE($.blobString) }])
+            $.OR([{ ALT: () => $.CONSUME(StringLiteral) }, { ALT: () => $.SUBRULE($.blobString) }])
         })
 
         $.RULE("blobString", () => {
@@ -237,7 +245,10 @@ class FREDParser extends Parser {
         })
 
         $.RULE("name", () => {
-            $.OR([{ ALT: $.CONSUME(Variable) }, { ALT: $.CONSUME(QuotedVariable) }])
+            $.OR([
+                { ALT: () => { $.CONSUME(Variable) } },
+                { ALT: () => { $.CONSUME(QuotedVariable) } }
+            ])
         })
 
         this.performSelfAnalysis()
@@ -247,18 +258,165 @@ class FREDParser extends Parser {
 // ONLY ONCE
 const parser = new FREDParser([])
 
-function parseInput(text) {
-    const lexingResult = FREDLexer.tokenize(text)
-    // "input" is a setter which will reset the parser's state.
-    parser.input = lexingResult.tokens
-    parser.value()
-    console.log(parser.errors)
 
-    if (parser.errors.length > 0) {
-        throw new Error("sad sad panda, Parsing errors detected")
+const BaseCstVisitor = parser.getBaseCstVisitorConstructor()
+
+class FREDToAstVisitor extends BaseCstVisitor {
+    constructor() {
+        super()
+        this.validateVisitor()
+    }
+
+    value(ctx) {
+        if (ctx.tagged != null) {
+            return { FREDValue: this.visit(ctx.tagged) }
+        }
+        if (ctx.atom != null) {
+            return { FREDValue: this.visit(ctx.atom) }
+        }
+    }
+
+
+    tagged(ctx) {
+        let tagName = this.visit(ctx.name)
+        let attrs = this.visit(ctx.attrs)
+        let atom = this.visit(ctx.atom)
+
+        return {
+            tag: tagName,
+            attrs: attrs,
+            atom: atom
+        }
+    }
+
+    atom(ctx) {
+        console.log(ctx)
+        if (ctx.object) {
+            return this.visit(ctx.object)
+        }
+        else if (ctx.array) {
+            return this.visit(ctx.array)
+        }
+        else if (ctx.dateOrDateTime) {
+            return this.visit(ctx.array)
+        }
+        else if (ctx.Time) {
+            return this.Time[0].image
+        }
+        else if (ctx.symbol) {
+            return this.visit(ctx.symbol)
+        }
+        else if (ctx.number) {
+            return this.visit(ctx.number)
+        }
+        else if (ctx.string) {
+            return this.visit(ctx.string)
+        }
+        else if (ctx.bool) {
+            console.log(ctx.bool)
+            return this.visit(ctx.bool)
+        }
+        else if (ctx.Null) {
+            return undefined
+        }
+    }
+
+    attrs(ctx) {
+        console.log(ctx.attr)
+
+        return ctx.attr.map(node => this.visit(node))
+    }
+    attr(ctx) {
+        console.log("entrou attr")
+        console.log(ctx.name)
+        console.log(ctx.atom)
+
+        let name = this.visit(ctx.name)
+        let atom = this.visit(ctx.atom)
+
+        return { name, atom }
+    }
+    object(ctx) {
+        return this.visit(ctx.pair)
+    }
+    pair(ctx) {
+        let key = this.visit(ctx.name)
+        let value = this.visit(ctx.value)
+        return { key, value }
+    }
+    array(ctx) {
+        return this.visit(ctx.atom)
+    }
+
+    bool(ctx) {
+        console.log(ctx)
+        if (ctx.True) {
+            console.log(ctx.True)
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    symbol(ctx) {
+        return "Symbol" + this.visit(ctx.name)
+    }
+    dateOrDateTime(ctx) {
+        return ctx.DateFormat[0].image + "T" + ctx.Time[0].image + ctx.TimeOffSet[0].image
+    }
+    number(ctx) {
+        if (ctx.NumberLiteral) {
+            return ctx.NumberLiteral[0].image
+        }
+        else if (ctx.HexLiteral) {
+            return ctx.HexLiteral[0].image
+        }
+        else if (ctx.OctLiteral) {
+            return ctx.OctLiteral[0].image
+        }
+        else if (ctx.BinLiteral) {
+            return ctx.BinLiteral[0].image
+        }
+    }
+    string(ctx) {
+        if (ctx.StringLiteral) {
+            return ctx.StringLiteral[0].image
+        }
+        else {
+            return this.visit(blobString)
+        }
+    }
+    blobString(ctx) {
+        return ctx.BlobString[0].image
+    }
+    name(ctx) {
+        if (ctx.Variable) {
+            console.log(ctx.Variable[0].image)
+            return ctx.Variable[0].image
+        }
+        else {
+            return ctx.QuotedVariable[0].image
+        }
     }
 
 }
 
-const inputText = "tag { key : \"str\"}"
+function parseInput(text) {
+    const lexingResult = FREDLexer.tokenize(text)
+    parser.input = lexingResult.tokens
+
+    const cst = parser.value()
+
+    const visitor = new FREDToAstVisitor()
+
+    const ast = visitor.visit(cst)
+    console.log("AST: " + ast)
+
+    if (parser.errors.length > 0) {
+        console.log(parser.errors)
+        throw new Error("sad sad panda, Parsing errors detected")
+    }
+}
+
+const inputText = "tag1 ;coment \n (h1=true h2=\"str\" ) {  ;coment \n key : \"str\"}"
 parseInput(inputText)
